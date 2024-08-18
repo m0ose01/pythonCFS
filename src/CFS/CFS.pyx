036024200835fa28cfa8e678ecc9b503705ca7e4
Seamus <m0ose01@proton.me>
2024-08-18 16:37:12 +0100

    Add get_chan_data function, and loadcfs script.
    Currently only works with int2 data.

---
+++ src/CFS/CFS.pyx
@@ -0,0 +1,41 @@
+from CFS import routines, constants
+from pathlib import Path
+import array
+import cython
+from cython.cimports.cpython import array
+
+# @cython.ccall
+def load_cfs(filename: bytes):
+    file_handle = routines.open_cfs_file(filename)
+    print(file_handle)
+
+    channel_count, file_variable_count, data_section_variable_count, data_section_count = routines.get_file_info(file_handle)
+
+    time, date, comment = routines.get_gen_info(file_handle)
+    message = (
+            f"File: {filename}\n"
+            f"Created on {date} at {time}\n"
+            f"Comment: {comment}\n"
+            f"{channel_count} channels, {file_variable_count} file variables, {data_section_variable_count} data section vars, {data_section_count} data sections."
+            )
+    print(message)
+
+    data = []
+
+    current_channel: cython.int = 0
+    for current_channel in range(channel_count):
+        current_channel_name, current_y_units, current_x_units, current_data_type, current_data_kind, spacing, other = routines.get_file_chan(file_handle, current_channel)
+        print(current_channel_name)
+        for current_data_section in range(data_section_count):
+            current_data_section: cython.int = 0
+
+            _, points, current_y_scale, current_y_offset, current_x_scale, current_x_offset = routines.get_ds_chan(file_handle, current_channel, current_data_section)
+
+            assert current_data_type == constants.CFSDataType.int_2
+
+            data = routines.get_chan_data(file_handle, current_channel, current_data_section, 0, points)
+            current_point: cython.int = 0
+            for current_point in range(points):
+                print(data[current_point])
+
+    routines.close_cfs_file(file_handle)
--- src/CFS/routines.pyx
+++ src/CFS/routines.pyx
@@ -1,6 +1,8 @@
+import array
 import cython
 from CFS.constants import CFSDataType, CFSDataKind
 from cython.cimports.CFS import api
+from cython.cimports.cpython import array
 
 @cython.ccall
 def open_cfs_file(filename: cython.p_char) -> api.cfs_short:
@@ -67,3 +69,13 @@ def get_ds_chan(handle: api.cfs_short, channel: api.cfs_short, data_section: api
     api.GetDSChan(handle, channel, data_section, &channel_offset, &points, &y_scale, &y_offset, &x_scale, &x_offset)
 
     return (channel_offset, points, y_scale, y_offset, x_scale, x_offset)
+
+@cython.ccall
+def get_chan_data(handle: api.cfs_short, channel: api.cfs_short, data_section: api.WORD, point_offset: api.cfs_long, points_to_read: api.WORD) -> array.array:
+    cdef array.array array_template = array.array('h', [])
+    cdef array.array data
+    data = array.clone(array_template, points_to_read, zero=True)
+    size_of_short_bytes = 2
+    area_size = points_to_read * size_of_short_bytes
+    api.GetChanData(handle, channel, data_section, point_offset, points_to_read, data.data.as_voidptr, area_size)
+    return data
